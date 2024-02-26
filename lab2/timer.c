84306c4734feeef83082647ca6a9459c5de38756
Guilherme Silva <gmpas4444@gmail.com>
2024-02-26 20:11:00 +0000

    Finished timer_get_conf and its auxs

--- lab2/timer.c
+++ lab2/timer.c
@@ -32,14 +32,16 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (st == NULL || timer <= 2) return 1;
 
-  return 1;
+  uint8_t rb_cmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+  sys_outb(TIMER_0 + timer, rb_cmd);
+  util_sys_inb(TIMER_0 + timer, st);
+
+  return 0;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
   /* To be implemented by the students */
   printf("%s is not yet implemented!\n", __func__);
 
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,24 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (lsb == NULL) return 1;
+  *lsb = val;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (msb == NULL) return 1;
+  *msb = (val >> 8);
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  // internamente usa-se o sys_inb, nao da para usar direto porque retorna 32 bits, e so estamos usar 8 no value
 
+  if (value == NULL) return 0;
+  uint32_t temp = 0x00000000;
+  sys_inb(port, temp);
+  *value = temp;
+  
   return 1;
-}
+  }
